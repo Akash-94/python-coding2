f49a98be66af97159354e19eaad5095bb64c389b
Jashwanth Gowda <56092435+Jashwanth-Gowda-R@users.noreply.github.com>
2019-10-20 22:16:28 +0530

    task to akash

---
+++ find error2.py
@@ -0,0 +1,10 @@
+Employee = ["Name": "John", "Age": 29, "salary":25000,"Company":"GOOGLE"]
+print(type(Employee))
+print("printing Employee data .... ")
+print("Name : %s" %Employee["Name"])
+print("Age : %d" %Employee["Age"])
+print("Salary : %d" %Employee["salary"])
+print("Company : %s" %Employee["Company"])
+
+# 1st remove the error from the above code
+# the try to update the entry by providing ur own entries by keyboard input and update the dictionary
